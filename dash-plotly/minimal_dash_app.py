c8a398a787636deb876fd67a7a8c3869122b1444
yegonbrian <yegonbrian.se@gmail.com>
2024-11-11 19:44:18 +0300

    Scrape YouTube Accounts

--- dash-plotly/minimal_dash_app.py
+++ dash-plotly/minimal_dash_app.py
@@ -1,28 +1,46 @@
-from dash import Dash, html, dcc, callback, Output, Input
-import plotly.express as px
+# Import packages
+from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
+import plotly.express as px
 
-# Import the data 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+# Incorporate data
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
 
-# Initialize the app
-app = Dash()
+# Initialize the app - incorporate css
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = Dash(external_stylesheets=external_stylesheets)
 
-# Define the app layout: 
+# App layout
 app.layout = [
-    html.H1(children='Title of the Dash App', style={'text-align':'center'}),
-    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
-    dcc.Graph(id='graph-content')
+    html.Div(className='row', children='My First App with Data, Graph, and Controls',
+             style={'textAlign': 'center', 'color': 'blue', 'fontSize': 30}),
+
+    html.Div(className='row', children=[
+        dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'],
+                       value='lifeExp',
+                       inline=True,
+                       id='my-radio-buttons-final')
+    ]),
+
+    html.Div(className='row', children=[
+        html.Div(className='six columns', children=[
+            dash_table.DataTable(data=df.to_dict('records'), page_size=11, style_table={'overflowX': 'auto'})
+        ]),
+        html.Div(className='six columns', children=[
+            dcc.Graph(figure={}, id='histo-chart-final')
+        ])
+    ])
 ]
 
+# Add controls to build the interaction
 @callback(
-    Output('graph-content', 'figure'), 
-    Input('dropdown-selection', 'value')
+    Output(component_id='histo-chart-final', component_property='figure'),
+    Input(component_id='my-radio-buttons-final', component_property='value')
 )
+def update_graph(col_chosen):
+    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
+    return fig
 
-def update_graph(value):
-    dff = df[df.country==value]
-    return px.line(dff, x='year', y='pop')
-
-if __name__ == "__main__":
-    app.run(debug=True, port=8052)
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
